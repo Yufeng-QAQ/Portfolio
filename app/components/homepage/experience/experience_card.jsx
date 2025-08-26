8f106c9dda116279c33072546614c349aefb01bc
Yufeng-QAQ <yufengli206@gmail.com>
2025-08-25 22:00:05 -0400

    Applied new experience design

--- app/components/homepage/experience/experience_card.jsx
+++ app/components/homepage/experience/experience_card.jsx
@@ -21,7 +21,7 @@ const ExperienceCard = ({ experience }) => {
       </div>
 
       {/* Job Title & Info */}
-      <p className="mt-2 text-white lg:text-xl">{experience.title}</p>
+      <p className="mt-2 text-green-400 lg:text-xl">{experience.title}</p>
       <div className="flex flex-col text-gray-400 text-xs md:text-sm lg:text-base">
         <div className="flex items-center space-x-2">
           <MdOutlineLocationOn />
@@ -34,15 +34,15 @@ const ExperienceCard = ({ experience }) => {
       </div>
 
       {/* Description */}
-      <div className="mt-3">
+      <div className="mt-3 text-gray-300">
         <p className="text-white text-sm md:text-md lg:text-lg">
           Key Achievements:
         </p>
-        <ul className="list-none pl-0 text-gray-300">
+        <ul className="list-none pl-0 ">
           {experience.achievements.map((achievement, index) => (
             <li
               key={index}
-              className="flex items-start gap-2 text-gray-400 text-xs md:text-sm lg:text-base mb-1"
+              className="flex items-start gap-2 text-gray-300 text-xs md:text-sm lg:text-base mt-2"
             >
               <span className="h-1 w-1 rounded-full bg-[#23950A] flex-shrink-0 mt-2"></span>
               <span className="flex-1">{achievement}</span>
--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -1,11 +1,11 @@
 import { experiences } from "@/utils/data/experience"; // Assuming you have project data
 import Image from "next/image";
-import ExpCard from "./exp-card.jsx"
+import ExpCard from "./exp-card.jsx";
 import ExperienceCard from "./experience_card.jsx";
 
 function Experience() {
   return (
-    <div id='expereince' className="relative z-50  my-12 lg:my-24">
+    <div id="expereince" className="relative z-50  my-12 lg:my-24">
       <div className="sticky top-10">
         <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
         <div className="flex items-center justify-start relative">
@@ -25,15 +25,15 @@ function Experience() {
               className="sticky-card w-full mx-auto max-w-2xl sticky"
             >
               <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
-                <ExpCard exp={experience} />
+                {/* <ExpCard exp={experience} /> */}
+                <ExperienceCard experience={experience} />
               </div>
-              <ExperienceCard experience={experience} />
             </div>
           ))}
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Experience;
